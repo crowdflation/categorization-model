217000ae1828e07551e973dcc970370a430d7b3b
mat <mat.bck@protonmail.com>
2021-09-29 01:45:20 +0200

    minor change

--- predict.py
+++ predict.py
@@ -8,21 +8,24 @@ from tensorflow.keras.layers import TextVectorization
 
 MAX_SEQUENCE_LENGTH = 50
 
-global label_classes, model
-
-with open("data/vocab.pickle", "rb") as fp:
-    vocab_data = pickle.load(fp)
-with open("data/label_classes.pickle", "rb") as fp:
-    label_classes = pickle.load(fp)
-
-vectorizer = TextVectorization(output_sequence_length=MAX_SEQUENCE_LENGTH, vocabulary=vocab_data)
-model = keras.models.load_model("data/models/model_saved_1")
+def load_pickle(file_path):
+    with open(file_path, "rb") as fp:
+        loaded_file = pickle.load(fp)
+    return loaded_file
 
 def predict(input_text):
     prediction = model.predict(input_text)
     argm_predict = np.argmax(prediction, axis=1)
     category = label_classes[argm_predict[0]]
     return category
+
+global label_classes, model
+
+vocab_data = load_pickle("data/vocab.pickle")
+label_classes = load_pickle("data/label_classes.pickle")
+
+vectorizer = TextVectorization(output_sequence_length=MAX_SEQUENCE_LENGTH, vocabulary=vocab_data)
+model = keras.models.load_model("data/models/model_saved_1")
 
 if __name__ == '__main__':
     text = sys.argv[1]
