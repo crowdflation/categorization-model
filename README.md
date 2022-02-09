eb68c29a07f19d69ed88de749dccf64f55e1adeb
mat <mat.bck@protonmail.com>
2022-02-09 17:32:29 +0100

    We now have a single unified model for all possible categories/major areas. subsequently the data dir is also been restructured.

--- README.md
+++ README.md
@@ -11,13 +11,12 @@ pip install -r requirements.txt
 
 Example of usage:
 ```bash
-python predict.py products.txt -c food_b -l en
+python predict.py products.txt -l en
 ```
 This example assumes that:
 1. you have a file 'products.txt' with raw data you want to make predictions about.
-2. the classes that you want to categorize the data into, are the ones belonging to the food&bevarage area.
-3. the examples are in english.
+2. the examples are in english.
 
-Just change the positional argument to whatever the path to your file is. Then use the '-c' flag to indicate which category that data belong to, and the '-l' flag to indicate the language.
+Just change the positional argument to whatever the path to your file is. Then use the '-l' flag to indicate the language.
 
-**N.B.** If you're not sure about what values you can use for the '-c' or '-l' flag, run `python predict.py -h` to have a list of possible choices.
+**N.B.** If you're not sure about which values you can use for the '-l' flag, run `python predict.py -h` to have a list of possible choices.
--- predict.py
+++ predict.py
@@ -21,23 +21,24 @@ def predict(array_product: np.ndarray):
     prediction_list = []
     predictions = model.predict(array_product)
     argm_predicts = np.argmax(predictions, axis=1)
+    confidence_scores = np.amax(predictions, axis=1)
     for argm_idx in argm_predicts:
         prediction_list.append(label_classes[argm_idx])
-    return prediction_list
+    return prediction_list, confidence_scores
 
 def open_target_file(file_path: str):
     product_list = []
-    with open(file_path, 'r', encoding = 'cp850') as fp:
+    with open(file_path, 'r', encoding = 'utf8') as fp:
         for line in fp:
             product_list.append(line.strip())
     return product_list
 
-def init_global_obj(category, lang):
+def init_global_obj(lang):
     config = load_config()
     global label_classes, model, vectorizer
-    vocab_data = load_pickle(config['vocab'][lang][category])
-    label_classes = load_pickle(config['label_classes'][lang][category])
-    model = keras.models.load_model(config['model'][lang][category])
+    vocab_data = load_pickle(config['vocab'][lang])
+    label_classes = load_pickle(config['label_classes'][lang])
+    model = keras.models.load_model(config['model'][lang])
     vectorizer = TextVectorization(output_sequence_length=MAX_SEQUENCE_LENGTH,
                                    vocabulary=vocab_data)
 
@@ -45,26 +46,22 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=str, metavar='path',
                         help="path to the target file that we want to get predictions from")
-    parser.add_argument("-c", "--category", required=True, type=str,
-                        help="the major category which the input data belong to",
-                        choices=['food_bev','apparel'])
     parser.add_argument("-l", "--language", required=True, type=str,
                         help="the language of the input data",
                         choices=['en','tr'])
     args = parser.parse_args()
 
-    category = args.category
     language = args.language
-    init_global_obj(category, language)
+    init_global_obj(language)
 
     output_json = {}
     target_file_path = args.data
     product_list = open_target_file(target_file_path)
     input_text = vectorizer(np.array([[product] for product in product_list])).numpy()
-    prediction_list = predict(input_text)
-    for product, prediction in zip(product_list, prediction_list):
-        output_json[product] = prediction
-    print(json.dumps(output_json, indent=2))
+    prediction_list, confidence_scores = predict(input_text)
+    for product, prediction, confidence_score in zip(product_list, prediction_list, confidence_scores):
+        output_json[product] = {"prediction":prediction, "confidence":round(confidence_score.item(), 4)}
+    print(json.dumps(output_json, indent=2, ensure_ascii=False))
 
 
 if __name__ == '__main__':
