5ed85a24592d743ab7314079250530bf083f40b7
mat <mat.bck@protonmail.com>
2022-02-22 00:48:53 +0100

    added the RequestBody class to validate request body data and for better docs.

--- my_app.py
+++ my_app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 import numpy as np
+from typing import List
+from pydantic import BaseModel
 
-#from public_repo.predict import init_global_obj
 from predict import init_global_obj, predict_f
 
+"""
+TODO
+- [x] implement the BaseClass object for data validation
+"""
+
+class RequestBody(BaseModel):
+    """
+    is this the right way to do this?
+    """
+    product_list: List[str]
 
 LANG = 'en'
 
@@ -17,9 +28,9 @@ def home():
     return {'ciao': 'Welcome to the API prediction'}
 
 @app.post('/predict')
-def predict(lang: str, request: dict):
+def predict(lang: str, request: RequestBody):
     json_response = {}
-    product_list = request['product_list']
+    product_list = request.product_list
     input_text = vectorizer(np.array([[product] for product in product_list])).numpy()
     prediction_list, confidence_scores = predict_f(model, input_text, label_classes)
     for product, prediction, confidence_score in zip(product_list, prediction_list, confidence_scores):
